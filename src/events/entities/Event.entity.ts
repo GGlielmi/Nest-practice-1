7a11c82ba503e4ac6701d6a81a6094da9ed0f96d
Guido Glielmi <gglielmi@baufest.com>
2024-05-18 16:38:35 -0300

    feat: decorated Event entity

--- src/events/entities/Event.entity.ts
+++ src/events/entities/Event.entity.ts
@@ -1,7 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Event {
+  @PrimaryGeneratedColumn('uuid') // with autoincrement
   id: number;
+
+  @Column()
   name: string;
+
+  @Column({ length: 100 })
   description: string;
-  when: string;
+
+  @Column()
+  when: Date;
+
+  @Column()
   address: string;
 }
