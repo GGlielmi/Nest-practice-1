dd479e1ddb88e8fb016f378369b267ae86fc1b39
Guido Glielmi <gglielmi@baufest.com>
2024-05-19 22:39:29 -0300

    feat: added validation for CreateEvent Dto

--- src/events/dtos/CreateEvent.dto.ts
+++ src/events/dtos/CreateEvent.dto.ts
@@ -1,6 +1,12 @@
+import { Length } from 'class-validator';
+
 export class CreateEventDto {
+  @Length(1)
   name: string = undefined;
+  @Length(1, 100)
   description: string = undefined;
+  @Length(1)
   when: Date = undefined;
+  @Length(1)
   address: string = undefined;
 }
